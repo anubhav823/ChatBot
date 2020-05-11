157cadc287975bf62f451a38fcf48d2a57c2c049
anubhav823 <30366878+anubhav823@users.noreply.github.com>
2020-05-11 10:54:35 +0530

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 16:49:00 2020
+
+@author: anubh
+"""
+
+response_a = "The {} has a gluten-free option, but it is not vegan"
+response_b = "We have a selection of sides to go along with the {}, including mashed potatoes and steamed vegatables."
+response_c = "{} includes habanero, so it is a bit spicy!"
+blank_spot = "food"
+
+
+responses = [response_a, response_b, response_c]
